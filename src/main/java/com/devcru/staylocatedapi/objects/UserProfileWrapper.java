6663fad31a65a41bbeb3a9e33445b4b17749119d
sephthompson <sephthompson@gmail.com>
2015-08-28 11:17:41 -0400

    Test profile creation with user creation 3

---
+++ src/main/java/com/devcru/staylocatedapi/objects/UserProfileWrapper.java
@@ -0,0 +1,21 @@
+package com.devcru.staylocatedapi.objects;
+
+public class UserProfileWrapper {
+	
+	User user;
+	Profile profile;
+	
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Profile getProfile() {
+		return profile;
+	}
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
+}
