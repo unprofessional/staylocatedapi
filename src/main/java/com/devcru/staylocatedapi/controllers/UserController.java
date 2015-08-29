06ee93d88a6ba1430ce65c4e8b7b0f8d8473e55f
sephthompson <sephthompson@gmail.com>
2015-08-29 19:46:11 -0400

    Test produces and consumes 4

--- src/main/java/com/devcru/staylocatedapi/controllers/UserController.java
+++ src/main/java/com/devcru/staylocatedapi/controllers/UserController.java
@@ -57,7 +57,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/", method=RequestMethod.POST,
-			produces="application/xml", consumes="application/json")
+			produces="application/json", consumes="application/json")
 	public @ResponseBody
 	JsonResponse registerUser(@RequestBody UserProfileWrapper requestWrapper) {
 		
